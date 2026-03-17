7e895044e0b8a6660b9c1bbaf189107e6bd4fcb4
amex-imd <ameximd@gmail.com>
2026-03-17 21:30:26 +0300

    The restore and modify methods were added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-#include <bytesbits.h>
+#include "bytesbits.h"
+#include <vector>
 
 int main()
 {
-    IMD::print_info<int>();
+    short val = 316;
+
+    IMD::BIG_ENDIAN::print_bin_bytes(val);
+    std::cout << std::endl;
+
+    std::vector<int> vec = {0, 0, 0, 0, 0, 0, 0, 1,
+                            0, 0, 1, 1, 1, 1, 0, 0};
+
+    std::cout << IMD::BIG_ENDIAN::restore_value_from_bits<short>(vec.begin(), vec.end());
+
     std::cin.get();
     return 0;
 }
